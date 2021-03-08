73cdafab8d5b29eb744f6431a2914dad1205ac75
Lewis <LButcher@qa.com>
2021-03-08 20:24:06 +0000

    Initial commit

---
+++ src/main/java/com/bae/equationSaverApp/EquationsAppApplication.java
@@ -0,0 +1,13 @@
+package com.bae.equationSaverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquationsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquationsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/equationSaverApp/EquationsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.equationSaverApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquationsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
