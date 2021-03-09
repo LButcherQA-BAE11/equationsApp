5598cc7deedf513bb581347a983e7a99054a7c48
Lewis <LButcher@qa.com>
2021-03-09 20:31:52 +0000

    Added in update and get equation by id functionality for EquationsController.java

--- src/main/java/com/bae/equationSaverApp/rest/EquationsController.java
+++ src/main/java/com/bae/equationSaverApp/rest/EquationsController.java
@@ -3,7 +3,9 @@ package com.bae.equationSaverApp.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,16 @@ public class EquationsController {
 	@GetMapping("/getAllEquations")
 	public List<Equations> getAllEquations() {
 		return this.service.getAllEquations();
+	}
+
+	@GetMapping("/getElephant/{id}")
+	public Equations getEquationById(@PathVariable Long id) {
+		return this.service.getEquationById(id);
+	}
+
+	@PutMapping("/update/{id}")
+	public Equations updateEquation(@PathVariable Long id, @RequestBody Equations newEquation) {
+
+		return this.service.updateEquation(id, newEquation);
 	}
 }
